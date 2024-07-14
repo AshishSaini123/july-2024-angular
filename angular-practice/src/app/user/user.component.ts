de2369dca47d096ee052b7a2d52fbcaf5164fd43
AshishSaini123 <aashucsengi@gmail.com>
2024-07-14 13:03:59 +0530

    created user component and added imgs

--- angular-practice/src/app/user/user.component.ts
+++ angular-practice/src/app/user/user.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component , signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-user';
 
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,4 +11,14 @@ import { Component } from '@angular/core';
 })
 export class UserComponent {
 
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  get imagePath() {
+    return 'assets/users/'+ this.selectedUser().avatar;
+  }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
 }
